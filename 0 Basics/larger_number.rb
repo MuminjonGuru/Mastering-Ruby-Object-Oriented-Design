7ef62e10b7d713d3e8dbb6f12a41f8d88f6e60c9
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-01-06 14:49:58 +0500

    Create larger_number.rb

---
+++ 0 Basics/larger_number.rb
@@ -0,0 +1,10 @@
+def larger_number(num1, num2)
+	if num1 > num2
+      return num1
+    else
+      return num2
+    end  
+end
+
+puts larger_number(42, 28)   # => 42
+puts larger_number(99, 100)  # => 100
