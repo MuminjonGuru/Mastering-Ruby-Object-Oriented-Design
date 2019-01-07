25bbde3b365bfcd831b615080ca5714a3e3dc6c3
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-01-07 23:09:47 +0500

    Create range.rb

---
+++ 0 Basics/range.rb
@@ -0,0 +1,12 @@
+def range(min, max)
+  numbers = []
+  while min <= max
+    numbers << min
+    min += 
+  end
+  return numbers
+end
+
+print range(2, 7)   # => [2, 3, 4, 5, 6, 7]
+puts
+print range(13, 20) # => [13, 14, 15, 16, 17, 18, 19, 20]
