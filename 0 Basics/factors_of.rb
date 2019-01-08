9c48e68fd31870f7781cf96296becd76205f641f
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-01-08 20:22:25 +0500

    Create factors_of.rb

---
+++ 0 Basics/factors_of.rb
@@ -0,0 +1,21 @@
+def factors_of(num)
+  factors = []
+  i = 1
+  while i <= num
+    if num % i == 0
+      factors << i
+    end 
+    i += 1
+  end  
+  return factors
+end
+
+print factors_of(3)   # => [1, 3]
+puts
+print factors_of(4)   # => [1, 2, 4]
+puts
+print factors_of(8)   # => [1, 2, 4, 8]
+puts
+print factors_of(9)   # => [1, 3, 9]
+puts
+print factors_of(16)  # => [1, 2, 4, 8, 16]
