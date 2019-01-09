d10729acb93a85f102c8c51ce6fe7897c8ba9e1e
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-01-09 23:39:49 +0500

    just some updates

--- 0 Basics/nested_loop_2.rb
+++ 0 Basics/nested_loop_2.rb
@@ -6,3 +6,18 @@ arr.each do |ele1|
   end
 end
 
+# only unique pairs
+
+arr = ["a", "b", "c", "d"] 
+
+arr.each_with_index do |ele1, idx1|
+  arr.each_with_index do |ele2, idx2|
+    if idx2 > idx1
+      puts ele1 + ele2
+      puts idx1.to_s + " " + idx2.to_s
+      puts "-----"
+    end
+  end
+end
+
+# if idx2 is greater than idx1, then idx2 is never referring to what idx1 referred to previously
