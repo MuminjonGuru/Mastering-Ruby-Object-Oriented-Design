f43f8acc03f1ab23d7a87ae06eb4a0db285dba44
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-01-13 22:26:43 +0500

    data structure - hashes

---
+++ 0 Basics/Hashes/first_try_hash.rb
@@ -0,0 +1,14 @@
+# Data Structure  - Hashes
+# key => value pairs
+
+my_hash = {
+  "name" => "App Academy", "color" => "red",
+  "age" => 5,
+  "isAwesome" => true,
+  43 => "hello"
+}
+
+my_hash["color"] = "black"
+my_hash["age"] += 1
+puts my_hash
+puts my_hash[43]
