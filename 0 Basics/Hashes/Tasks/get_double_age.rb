e99b0f5364772c0d3875ac71c3b04ad78447e216
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-01-15 00:00:43 +0500

    Create get_double_age.rb

---
+++ 0 Basics/Hashes/Tasks/get_double_age.rb
@@ -0,0 +1,6 @@
+def get_double_age(hash)
+  return hash["age"] * 2
+end
+
+puts get_double_age({"name"=>"App Academy", "age"=>5}) # => 10
+puts get_double_age({"name"=>"Ruby", "age"=>23})       # => 46
