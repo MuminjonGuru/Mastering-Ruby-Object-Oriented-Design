6099e99621669ef1128116292aab77c2df34af7d
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-01-14 23:50:25 +0500

    Create def_value.rb

---
+++ 0 Basics/Hashes/def_value.rb
@@ -0,0 +1,11 @@
+# nil 
+#   represents "nothing" or "empty"
+#   common appears as a default value
+
+# my_array = ["a", "b", "c"]
+# print my_array[0]
+
+my_hash = {"name" => "Bob", "age" => 3}
+p my_hash["location"]
+
+# in the ruby language default value is nill. That is it)
