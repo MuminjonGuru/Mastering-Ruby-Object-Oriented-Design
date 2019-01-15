cde1700374338851991d2dd67daa75785d9644c8
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-01-16 00:08:15 +0500

    Create get_full_name_from_hash.rb

---
+++ 0 Basics/Hashes/Tasks/get_full_name_from_hash.rb
@@ -0,0 +1,9 @@
+def get_full_name(hash)
+  return hash["first"] + " " + hash["last"] + ", the " + hash["title"]
+end
+
+hash1 = {"first"=>"Michael", "last"=>"Jordan", "title"=> "GOAT"}
+puts get_full_name(hash1) # => "Michael Jordan, the GOAT"
+
+hash2 = {"first"=>"Fido", "last"=>"McDog", "title"=> "Loyal"}
+puts get_full_name(hash2) # => "Fido McDog, the Loyal"
