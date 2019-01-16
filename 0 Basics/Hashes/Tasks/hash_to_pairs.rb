1d3b4ae7a0f82de477469095c590029a2db909c4
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-01-16 15:56:33 +0500

    Create  hash_to_pairs.rb

---
+++ 0 Basics/Hashes/Tasks/hash_to_pairs.rb
@@ -0,0 +1,10 @@
+def hash_to_pairs(hash)
+  pairs = []
+  hash.each { |key, val | pairs << [key, val]}
+  return pairs
+end
+
+print hash_to_pairs({"name"=>"skateboard", "wheels"=>4, "weight"=>"7.5 lbs"}) #=> [["name", "skateboard"], ["wheels", 4], ["weight", "7.5 lbs"]]
+puts
+print hash_to_pairs({"kingdom"=>"animalia", "genus"=>"canis", "breed"=>"German Shepherd"}) #=> [["kingdom", "animalia"], ["genus", "canis"], ["breed", "German Shepherd"]]
+puts
