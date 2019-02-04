ebf9bd076e054c51edf4af7f7a2f82501edad409
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-02-05 01:35:50 +0500

    Create map_by_name.rb

---
+++ 0 Basics/Maps/map_by_name.rb
@@ -0,0 +1,22 @@
+def map_by_name(arr)
+ return arr.map { |hash|  hash["name"] }
+end
+
+
+pets = [
+  {"type"=>"dog", "name"=>"Rolo"},
+  {"type"=>"cat", "name"=>"Sunny"},
+  {"type"=>"rat", "name"=>"Saki"},
+  {"type"=>"dog", "name"=>"Finn"},
+  {"type"=>"cat", "name"=>"Buffy"}
+]
+print map_by_name(pets) #=> ["Rolo", "Sunny", "Saki", "Finn", "Buffy"]
+puts
+
+countries = [
+ {"name"=>"Japan", "continent"=>"Asia"},
+ {"name"=>"Hungary", "continent"=>"Europe"},
+ {"name"=>"Kenya", "continent"=>"Africa"},
+]
+print map_by_name(countries) #=> ["Japan", "Hungary", "Kenya"]
+puts
