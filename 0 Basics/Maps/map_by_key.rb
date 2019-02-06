d5c1eceaf60819f0c8b3bfda22a897dfaa840b79
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-02-06 15:04:11 +0500

    Create map_by_key.rb

---
+++ 0 Basics/Maps/map_by_key.rb
@@ -0,0 +1,14 @@
+def map_by_key(arr, key)
+	return arr.map { |hash| hash[key] }
+end
+
+locations = [
+  {"city"=>"New York City", "state"=>"New York", "coast"=>"East"},
+  {"city"=>"San Francisco", "state"=>"California", "coast"=>"West"},
+  {"city"=>"Portland", "state"=>"Oregon", "coast"=>"West"},
+]
+
+print map_by_key(locations, "state") #=> ["New York", "California", "Oregon"]
+puts
+print map_by_key(locations, "city") #=> ["New York City", "San Francisco", "Portland"]
+puts
