5a86f5d392fd3b0f37c54f819e56c91e1ac05498
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-03-08 16:08:10 +0500

    Create 01_coprime.rb

---
+++ Challenges/muminjon-abduraimov-advanced_methods_exercise/01_coprime.rb
@@ -0,0 +1,18 @@
+# Write a method, coprime?(num_1, num_2), that accepts two numbers as args.
+# The method should return true if the only common divisor between the two numbers is 1.
+# The method should return false otherwise. For example coprime?(25, 12) is true because
+# 1 is the only number that divides both 25 and 12.
+
+def coprime?(n1, n2)
+  res1 = n1.gcd(n2)
+  res2 = n2.gcd(n1)
+
+  return true if res1 == 1 && res2 == 1
+
+  false
+end
+
+p coprime?(25, 12)    # => true
+p coprime?(7, 11)     # => true
+p coprime?(30, 9)     # => false
+p coprime?(6, 24)     # => false
