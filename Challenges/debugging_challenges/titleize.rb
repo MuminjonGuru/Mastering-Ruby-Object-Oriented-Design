864a5f09928d5661c324a945e2b3ce9fd59cffde
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-03-15 13:27:21 +0500

    Add files via upload

---
+++ Challenges/debugging_challenges/titleize.rb
@@ -0,0 +1,22 @@
+# Debug this code to pass rspec! There are 2 mistakes to fix.
+
+# Write a method, titleize, that accepts a string representing a title 
+# and capitalizes each word in a string except 'a', 'and', 'of', 'on', or 'the'.
+# The first word of the title should be capitalized no matter what.
+
+require "byebug"
+
+def titleize(title)
+  little_words = [ "and", "the", "over", "a", "on", "of" ]
+  words = title.split(" ")
+
+  titleized_words = words.map.with_index do |word, i|
+    if i == 0 || !little_words.include?(word)
+      word.capitalize
+    else
+      word.downcase
+    end
+  end
+
+  titleized_words.join(" ")
+end
