eb1ee936caebaa5b9ecb166c537e6dcb975ea9cd
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-05-27 19:12:53 +0500

    histogram problem solved

---
+++ 0 Basics/histogram.rb
@@ -0,0 +1,14 @@
+puts "Text please: "
+text = gets.chomp
+
+text = text.split(" ")
+
+frequencies = Hash.new(0)
+
+words.each { |word| frequencies[word] += 1 }
+
+frequencies = frequencies.sort_by {|a, b| b}
+
+frequencies.reverse!
+
+frequencies.each { |word, frequency| puts word + " " + frequency.to_s }
