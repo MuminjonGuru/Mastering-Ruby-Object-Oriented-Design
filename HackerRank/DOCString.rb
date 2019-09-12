7d884c0c375bc464b2b616d6fcaa6b9d660dc3bf
Abduraimov Muminjon <muminjonguru@outlook.com>
2019-09-12 18:54:14 +0500

    DOC String

---
+++ HackerRank/DOCString.rb
@@ -0,0 +1,18 @@
+def single_quote
+  'Hello World and others!'
+end
+
+def double_quote
+  "Hello World and others!"
+end
+
+def here_doc
+   <<-EOF
+    Hello World and others! 
+    test
+    test2
+    test3
+    test4
+    test5
+    EOF
+end
